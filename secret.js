e8def95736f45c30135e40379545915267de3f47
Jacob Ville <VilleLJacob@gmail.com>
2017-02-24 16:08:54 -0500

    Use sergio's API key

--- secret.js
+++ secret.js
@@ -1,1 +1,2 @@
-module.exports = { API_KEY: 'fc39d621df364e14ab2c316336cd3cdf' };
+// module.exports = { API_KEY: 'fc39d621df364e14ab2c316336cd3cdf' };
+module.exports = { API_KEY: '7770f947a5894ef894edee206306fc0a' };
