bf77d71a892e3f08ecb0496919f062dc0fd674c7
Jacob Ville <VilleLJacob@gmail.com>
2017-02-28 16:06:41 -0500

    Use valid API KEY from TA

--- secret.js
+++ secret.js
@@ -1,2 +1,2 @@
 // module.exports = { API_KEY: 'fc39d621df364e14ab2c316336cd3cdf' };
-module.exports = { API_KEY: '7770f947a5894ef894edee206306fc0a' };
+module.exports = { API_KEY: '74857aa273724df793371c715124228d' };
